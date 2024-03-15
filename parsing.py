b461c4cfaef5f5d89a23a8d371d6e1696bc21934
Nika Zyryanova <nika.zyryanova.05@mail.ru>
2024-03-15 14:34:42 +0500

    second commit

--- parsing.py
+++ parsing.py
@@ -1,6 +1,9 @@
 import requests
 from bs4 import BeautifulSoup as bs
-website = requests.get('https://russiandrone.ru/publications/?show_by=100')
+website = requests.get('https://russiandrone.ru/publications/?show_by=100').text
 website_bs = bs('website', 'lxml')
-inf = website_bs.find('selection', class_='priducts')
+inf = website_bs.find("section", class_="products").find("div", class_="desc")
 print(inf)
+
+
+#как-то использовать product.name
