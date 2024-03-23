722b25168b31a0a6320ad982ca6e0780eeefff77
Nika Zyryanova <nika.zyryanova.05@mail.ru>
2024-03-23 21:33:19 +0500

    нужные теги с данными код не видит

--- parsing.py
+++ parsing.py
@@ -4,7 +4,6 @@ website = requests.get('https://russiandrone.ru/publications/?show_by=100')
 website_bs = bs(website.text, 'lxml')
 #print(website_bs) #чтение html кода верно
 #inf = website_bs.select("div.name.a")
-#inf = website_bs.find("section",class_="content right")
 pars = website_bs.find('div', class_ = 'left-menu-bl').find('div', class_='cats left-menu-block')
 print(pars)
 for item in pars:
